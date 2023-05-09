79210a46ded6511df0a003b256b7b8fe8b080753
Dimitrije Dobrijevic <dimitrije.dobrijevic@gmail.com>
2023-05-09 16:28:49 +0200

    Uradjena stilizacija za kontakt stranicu

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -3,19 +3,21 @@ import {useForm} from 'react-hook-form';
 const Contact = () => {
 
     const {register, handleSubmit} = useForm();
-    const onSubmit = (d) =>
-    alert(['Uspesno ste poslali podatke', JSON.stringify(d)]);
+    const onSubmit = (d) => {
+        alert(['Uspesno ste poslali podatke', JSON.stringify(d)]);
+        console.log(d.firstname);
+    }
 
     return (
         <div>
             <h3>Ukoliko zelite da budete obavestavani o rezultatima, molim vas popunite formu.</h3>
             <div>
                 <form onSubmit={handleSubmit(onSubmit)}>
-                    <input type="text" {...register("firstname")} placeholder="Ime"></input>
-                    <input type="text" {...register("lastname")} placeholder="Prezime"></input>
-                    <input type="date" {...register("birth")}></input>
-                    <input type="email" {...register("email")} placeholder="Email"></input>
-                    <input type="submit" value="Posalji podatke"></input>
+                    <input type="text" {...register("firstname", {required: true})} placeholder="Ime"></input>
+                    <input type="text" {...register("lastname", {required: true})} placeholder="Prezime"></input>
+                    <input type="date" {...register("birth", {required: true})}></input>
+                    <input type="email" {...register("email", {required: true})} placeholder="Email"></input>
+                    <input type="submit" value="Posalji podatke" id='contactSubmit'></input>
                 </form>
             </div>
         </div>
